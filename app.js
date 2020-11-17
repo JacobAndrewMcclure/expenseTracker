d3243085c649369dde6bf5b5946416da4d7e1450
Jacob McClure <jacobmcclure801@gmail.com>
2020-11-17 13:10:45 -0700

    commit on master

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ let rowElement;
 let vendorNode;
 let dateNode;
 let amountNode;
+let btnNode;
 
 let vendorTextNode;
 let dateTextNode;
 let amountTextNode;
+let btnTextNode;
 
 function addItem() {
     //take user input & assign to variable
@@ -25,20 +27,24 @@ function addItem() {
     vendorNode = document.createElement("TD");
     dateNode = document.createElement("TD");
     amountNode = document.createElement("TD");
+    btnNode = document.createElement("BUTTON");
     //create tr element
     rowElement = document.createElement("TR");
     //create text node to store user inputs
     vendorTextNode = document.createTextNode(vendor);
     dateTextNode = document.createTextNode(date);
     amountTextNode = document.createTextNode("$" + amount);
+    btnTextNode = document.createTextNode("X");
     //apped userinputs to TD element
     vendorNode.appendChild(vendorTextNode);
     dateNode.appendChild(dateTextNode);
     amountNode.appendChild(amountTextNode);
+    btnNode.appendChild(btnTextNode);
     //append TD elements to TR element
     rowElement.appendChild(vendorNode);
     rowElement.appendChild(dateNode);
     rowElement.appendChild(amountNode);
+    rowElement.appendChild(btnNode);
     //append TD element to table section
     document.getElementById("expenseTable").appendChild(rowElement);
 }
